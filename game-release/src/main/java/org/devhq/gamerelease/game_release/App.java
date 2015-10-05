460e5f55abf7d535f36c1bc66e30222e4f1f3130
Haitham Qamhiyeh <hqamhiyeh@gmail.com>
2015-10-04 23:57:35 -0500

    Initial commit

---
+++ game-release/src/main/java/org/devhq/gamerelease/game_release/App.java
@@ -0,0 +1,13 @@
+package org.devhq.gamerelease.game_release;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
